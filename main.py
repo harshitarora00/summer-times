c139a46af9d403b077f201a9f65a22c146d4fb71
Harshit Arora <94703188+harshitarora00@users.noreply.github.com>
2025-02-28 23:16:10 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+temp = int(input("What is the temperature today:"))
+if temp > 15:
+  print("It's a hot day. You can wear light clothes.")
+else:
+  print("It's a cold day. You should wear warm clothes.")
